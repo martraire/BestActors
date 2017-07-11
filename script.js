329092ee71538a4c39a2950b3d9cf025cc980d8e
Diane Martraire <diane.martraire@gmail.com>
2017-07-11 14:05:11 +0200

    Translate menu into english

--- script.js
+++ script.js
@@ -6,16 +6,38 @@ $(function(){
     
     init = function(){
       $mainMenuItems.children(".images").click(function(){
-          var newIndex = $(this).parent().index(),
-          $item = $mainMenuItems.eq(newIndex),
-          $colorImage = $item.find(".color");
+          var newIndex = $(this).parent().index();
+          $item = $mainMenuItems.eq(newIndex);
+          if(openedIndex === newIndex)
+          {
+              animateItem($item, false, 250);
+              openedIndex = -1;
+          }
+          else
+          {
+                if(validIndex(newIndex))
+                {
+                    animateItem($mainMenuItems.eq(openedIndex), false, 250);
+                    openedIndex = newIndex;
+                    animateItem($item, true, 250);
+                }
+          }
           
-          $colorImage.animate({left:"0px"},250);
-          $item.animate({width: "420px"}, 250);
-          
-          openedIndex = newIndex;
-          });
-    };
+      });
+    },
+        
+    validIndex = function(indexToCheck){
+        return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems)
+    },
+    
+    animateItem = function($item, toOpen, speed){
+        var $colorImage = $item.find(".color"),
+        itemParam = toOpen ? {width: "420px"}: {width: "140px"},
+        colorImageParam = toOpen ? {left: "0px"}: {left: "140px"};
+        $colorImage.animate(colorImageParam, speed);
+        $item.animate(itemParam, speed);
+    };    
+    
     init();
         
     
