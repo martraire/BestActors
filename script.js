78bce0d1d02812c52114211c0c7ed15e51586635
Diane Martraire <diane.martraire@gmail.com>
2017-07-17 22:48:16 +0200

    Reorganize the JS code and opening animation

--- script.js
+++ script.js
@@ -2,31 +2,37 @@ $(function(){
     
     var $mainMenuItems = $("#main-menu ul").children("li"),
     totalMainMenuItems = $mainMenuItems.length,
-    openedIndex = -1,
+    openedIndex = 2,
     
     init = function(){
-      $mainMenuItems.children(".images").click(function(){
-          var newIndex = $(this).parent().index();
-          $item = $mainMenuItems.eq(newIndex);
-          if(openedIndex === newIndex)
-          {
-              animateItem($item, false, 250);
-              openedIndex = -1;
-          }
-          else
-          {
+        bindEvents();
+        
+        if(validIndex(openedIndex))
+                animateItem($mainMenuItems.eq(openedIndex), true, 700);
+    },
+    
+    bindEvents = function(){
+        $mainMenuItems.children(".images").click(function(){
+            var newIndex = $(this).parent().index();
+            $item = $mainMenuItems.eq(newIndex);
+            if(openedIndex === newIndex)
+            {
+                animateItem($item, false, 250);
+                openedIndex = -1;
+            }
+            else
+            {
                 if(validIndex(newIndex))
                 {
                     animateItem($mainMenuItems.eq(openedIndex), false, 250);
                     openedIndex = newIndex;
                     animateItem($item, true, 250);
                 }
-          }
+            }
           
-      });
-    },
-    
-    
+        });
+        },
+        
     validIndex = function(indexToCheck){
         return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems)
     },
